f29d0e5f742d43c968067b46b2ad97ba23889f35
Cameron <chjm655@gmail.com>
2019-12-04 10:51:02 -0500

    TODO in csim
    A bunch of boilerplate code

--- csim.c
+++ csim.c
@@ -1,7 +1,21 @@
 #include "cachelab.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+	// TODO: Get the cmd arguments (flags, trace file)
     printSummary(0, 0, 0);
     return 0;
+}
+
+struct cache {
+	// TODO: Make a struct with v, tag and byte
+};
+
+struct request {
+	// TODO: store if a request was a hit or miss and what was requested (not sure if this struct is necessary)
+};
+
+int getCache(int* address) {
+	// TODO: Request info from the cache, mark a hit or miss, and updates the cache accordingly
+	return 0;
 }
