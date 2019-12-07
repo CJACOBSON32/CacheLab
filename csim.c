98239559c3b55ed5171717228cfeb58e15e54060
aniartist <wsly@wpi.edu>
2019-12-07 13:24:03 -0500

    Added guiding comments & general struct for cache

--- csim.c
+++ csim.c
@@ -2,6 +2,14 @@
 
 typedef struct {
 
+	int sets;
+	int lines;
+	int blocks;
+
+} generalCache;
+
+typedef struct {
+
 	char valid;
 	int tag;
 	int LRU_counter;
@@ -16,9 +24,13 @@ typedef struct {
 
 } cache_summary;
 
+//Method for LRU policy(?)
+
 int main(int argc, char *argv[])
 {
+	//Cache size = S * E * B
 	int opt;
+	char *fileName;
 	while((opt = getopt(argc,argv,"s:E:b:t:vh")) != -1) {
 
 		switch(opt) {
@@ -41,8 +53,14 @@ int main(int argc, char *argv[])
 		default:
 			return 0;
 		}
-
 	}
+	//Parse the file: L,M, S
+	File *traceFile = fopen(fileName, "r");
+
+	//If valid bit = 0, miss
+	//Otherwise, if tag bit of address = tag bit of cache, hit
+	//If neither miss or hit, it is evicted
+
 	// TODO: Get the cmd arguments (flags, trace file)
 	printSummary(0, 0, 0);
 	return 0;
