54456d4a71e61b0e633ea7836401c012a257debc
aniartist <wsly@wpi.edu>
2019-12-07 12:29:28 -0500

    Added structs

--- csim.c
+++ csim.c
@@ -1,10 +1,26 @@
 #include "cachelab.h"
 
+typedef struct {
+
+	char valid;
+	int tag;
+	int LRU_counter;
+
+} cache_line;
+
+typedef struct {
+
+	int hit;
+	int miss;
+	int eviction;
+
+} cache_summary;
+
 int main(int argc, char *argv[])
 {
 	// TODO: Get the cmd arguments (flags, trace file)
-    printSummary(0, 0, 0);
-    return 0;
+	printSummary(0, 0, 0);
+	return 0;
 }
 
 struct cache {
