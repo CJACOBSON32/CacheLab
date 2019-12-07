c96086a4c9beb82751547ded905b19befe61a359
Cameron <chjm655@gmail.com>
2019-12-07 12:30:34 -0500

    Removed my structs

--- csim.c
+++ csim.c
@@ -23,14 +23,6 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-typedef struct cache {
-	// TODO: Make a struct with v, tag and byte
-};
-
-typedef struct request {
-	// TODO: store if a request was a hit or miss and what was requested (not sure if this struct is necessary)
-};
-
 int getCache(int* address) {
 	// TODO: Request info from the cache, mark a hit or miss, and updates the cache accordingly
 	return 0;
