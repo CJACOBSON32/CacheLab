b2494280ca645151521f178d34f3f90f0351355f
Cameron <chjm655@gmail.com>
2019-12-07 13:50:34 -0500

    Made some functions for cache
    Also did some stuff to gitignore

--- csim.c
+++ csim.c
@@ -8,13 +8,19 @@ typedef struct {
 
 } cache_line;
 
-typedef struct {
+typedef enum {
 
-	int hit;
-	int miss;
-	int eviction;
+	hit,
+	miss,
+	eviction,
+	none
 
 } cache_summary;
+
+int S;
+int E;
+
+cache_line cache[][];
 
 int main(int argc, char *argv[])
 {
@@ -23,7 +29,49 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-cache_summary getCache(int* address) {
-	// TODO: Request info from the cache, mark a hit or miss, and updates the cache accordingly
+cache_summary* getCache(char type, int address, int size) {
+	// Request info from the cache, mark a hit or miss, and update the cache accordingly
+	cache_summary result[2] = 0;
+
+	switch(type) {
+		case 'L':
+			result = load(address, size);
+			break;
+		case 'S':
+			result = store(address, size);
+			break;
+		case 'M':
+			result = modify(address, size);
+			break;
+	}
+
+	return result;
+}
+
+cache_summary* load(int address, int size) {
+	// Load without changing memory, if there is a cache hit, return summary.
+
+
+	// If there is a miss, load into cache and return summary
+
+
 	return 0;
 }
+
+cache_summary* store(int address, int size) {
+	// Store into main memory and cache
+
+	return 0;
+}
+
+cache_summary* modify(int address, int size) {
+
+	// Record the result of a load and a store
+	cache_summary sum1 = load(address, size);
+	cache_summary sum2 = store(address, size);
+
+	cache_summary sums[2] = {sum1,sum2};
+
+	return 0;
+}
+
