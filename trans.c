1071c3d138e60e8275b9bffde106bea212996ad9
aniartist <wsly@wpi.edu>
2019-12-11 08:55:48 -0500

    Added comments & changed variable names

--- trans.c
+++ trans.c
@@ -108,63 +108,63 @@ void transpose32(int M, int N, int A[N][M], int B[M][N]) {
 char trans_desc64[] = "Transpose of a 64x64 matrix";
 void transpose64(int M, int N, int A[M][N], int B[M][N]) {
 
-	int i, j, k, l, block0, block1, block2, block3, block4, block5, block6, block7;
+	int i, j, blockRow, blockCol, block0, block1, block2, block3, block4, block5, block6, block7;
 
 	for (i = 0; i < N; i += 8) {
 
 		for (j = 0; j < M; j += 8) {
 
-			//First for loop goes through the top half
-			for (k = i; k < i + 4; k++) {
+			//First for loop goes through the top half of an 8x8 matrix (goes through a 4x8 matrix)
+			for (blockRow = i; blockRow < i + 4; blockRow++) {
 
-				block0 = A[k][j];
-				block1 = A[k][j + 1];
-				block2 = A[k][j + 2];
-				block3 = A[k][j + 3];
-				block4 = A[k][j + 4];
-				block5 = A[k][j + 5];
-				block6 = A[k][j + 6];
-				block7 = A[k][j + 7];
+				block0 = A[blockRow][j];
+				block1 = A[blockRow][j + 1];
+				block2 = A[blockRow][j + 2];
+				block3 = A[blockRow][j + 3];
+				block4 = A[blockRow][j + 4];
+				block5 = A[blockRow][j + 5];
+				block6 = A[blockRow][j + 6];
+				block7 = A[blockRow][j + 7];
 
 				//Transposing every 4 blocks
-				B[j][k] = block0;
-				B[j + 1][k] = block1;
-				B[j + 2][k] = block2;
-				B[j + 3][k] = block3;
-				B[j][k + 4] = block4;
-				B[j + 1][k + 4] = block5;
-				B[j + 2][k + 4] = block6;
-				B[j + 3][k + 4] = block7;
+				B[j][blockRow] = block0;
+				B[j + 1][blockRow] = block1;
+				B[j + 2][blockRow] = block2;
+				B[j + 3][blockRow] = block3;
+				B[j][blockRow + 4] = block4;
+				B[j + 1][blockRow + 4] = block5;
+				B[j + 2][blockRow + 4] = block6;
+				B[j + 3][blockRow + 4] = block7;
 			}
 
-			//Second inner for loop goes through the bottom half
-			for (l = j + 4; l < j + 8; l++) {
+			//Second inner for loop goes through the bottom half of an 8x8 matrix (goes through a 4x8 matrix)
+			for (blockCol = j + 4; blockCol < j + 8; blockCol++) {
 
-				block4 = A[i + 4][l - 4];
-				block5 = A[i + 5][l - 4];
-				block6 = A[i + 6][l - 4];
-				block7 = A[i + 7][l - 4];
+				block4 = A[i + 4][blockCol - 4];
+				block5 = A[i + 5][blockCol - 4];
+				block6 = A[i + 6][blockCol - 4];
+				block7 = A[i + 7][blockCol - 4];
 
-				block0 = B[l - 4][i + 4];
-				block1 = B[l - 4][i + 5];
-				block2 = B[l - 4][i + 6];
-				block3 = B[l - 4][i + 7];
+				block0 = B[blockCol - 4][i + 4];
+				block1 = B[blockCol - 4][i + 5];
+				block2 = B[blockCol - 4][i + 6];
+				block3 = B[blockCol - 4][i + 7];
 
-				B[l - 4][i + 4] = block4;
-				B[l - 4][i + 5] = block5;
-				B[l - 4][i + 6] = block6;
-				B[l - 4][i + 7] = block7;
+				B[blockCol - 4][i + 4] = block4;
+				B[blockCol - 4][i + 5] = block5;
+				B[blockCol - 4][i + 6] = block6;
+				B[blockCol - 4][i + 7] = block7;
 
 				//Transposing every 4 blocks
-				B[l][i] = block0;
-				B[l][i + 1] = block1;
-				B[l][i + 2] = block2;
-				B[l][i + 3] = block3;
+				B[blockCol][i] = block0;
+				B[blockCol][i + 1] = block1;
+				B[blockCol][i + 2] = block2;
+				B[blockCol][i + 3] = block3;
 
-				B[l][i + 4] = A[i + 4][l];
-				B[l][i + 5] = A[i + 5][l];
-				B[l][i + 6] = A[i + 6][l];
-				B[l][i + 7] = A[i + 7][l];
+				B[blockCol][i + 4] = A[i + 4][blockCol];
+				B[blockCol][i + 5] = A[i + 5][blockCol];
+				B[blockCol][i + 6] = A[i + 6][blockCol];
+				B[blockCol][i + 7] = A[i + 7][blockCol];
 
 			}
 		}
